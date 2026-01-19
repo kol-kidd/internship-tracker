6bf4be81bf6a069a4675cf851771452c68608810
kol-kidd <uyancole@gmail.com>
2026-01-19 14:22:52 +0800

    Add Vercel Analytics to main entry point
    Imported and rendered the Analytics component from @vercel/analytics/next in the main application file to enable analytics tracking.

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <App />
+        <Analytics />
       </LocalizationProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
